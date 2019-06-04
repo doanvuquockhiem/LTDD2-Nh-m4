d2315c777e0837f6b79df80e26169ddb814ce6ce
doanvuquockhiem <33826975+doanvuquockhiem@users.noreply.github.com>
2019-06-04 16:13:25 +0700

    Add files via upload

---
+++ Đồ Án LTDD2/java/com/ae4/nhom_4/ContactModel.java
@@ -0,0 +1,39 @@
+package com.ae4.nhom_4;
+
+
+
+public class ContactModel {
+    private int giaDatCuocChinh;
+    private int imageCon;
+    private String tenCon;
+
+    public ContactModel(int giaDatCuocChinh, int imageCon, String tenCon) {
+        this.giaDatCuocChinh = giaDatCuocChinh;
+        this.imageCon = imageCon;
+        this.tenCon = tenCon;
+    }
+
+    public int getGiaDatCuocChinh() {
+        return giaDatCuocChinh;
+    }
+
+    public void setGiaDatCuocChinh(int giaDatCuocChinh) {
+        this.giaDatCuocChinh = giaDatCuocChinh;
+    }
+
+    public int getImageCon() {
+        return imageCon;
+    }
+
+    public void setImageCon(int imageCon) {
+        this.imageCon = imageCon;
+    }
+
+    public String getTenCon() {
+        return tenCon;
+    }
+
+    public void setTenCon(String tenCon) {
+        this.tenCon = tenCon;
+    }
+}
